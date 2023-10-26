94a7fb98c9b103c436a46bcffdca55c1f8cefbf6
Ferhat Çıkrık <ferhatckrk@gmail.com>
2023-10-27 00:37:49 +0300

    first commit

---
+++ app/src/main/java/com/example/ecommercekotlin/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommercekotlin.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommercekotlin.R
+
+class IntroductionFragment : Fragment(R.layout.fragment_introdcution) {
+}
---
+++ app/src/main/java/com/example/ecommercekotlin/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,6 @@
+package com.example.ecommercekotlin.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommercekotlin.R
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
