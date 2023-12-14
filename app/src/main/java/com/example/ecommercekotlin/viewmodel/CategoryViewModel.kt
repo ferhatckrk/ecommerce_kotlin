4cc7a0bce1501fc1c3d9e328d6c87d1d50d646f6
Ferhat Çıkrık <ferhatckrk@gmail.com>
2023-12-15 01:45:02 +0300

    added ColorsAdaprer,SizeAdapter,Category Data,Product Details Fragment, CategoryViewMdoel

---
+++ app/src/main/java/com/example/ecommercekotlin/viewmodel/CategoryViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.ecommercekotlin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ecommercekotlin.data.Category
+import com.example.ecommercekotlin.data.Product
+import com.example.ecommercekotlin.utils.Resource
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObjects
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class CategoryViewModel constructor(
+    private val firestore: FirebaseFirestore,
+    private val category: Category
+) : ViewModel() {
+
+    private val _offerProduct = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val offerProducts = _offerProduct.asStateFlow()
+
+    private val _bestProduct = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val bestsProducts = _bestProduct.asStateFlow()
+
+
+
+    init {
+        fetchBestsProduct()
+        fetchOfferProduct()
+    }
+
+    fun fetchOfferProduct() {
+        viewModelScope.launch {
+            _offerProduct.emit(Resource.Loading())
+        }
+        firestore.collection("Products").whereEqualTo("category", category.category)
+            .whereNotEqualTo("offerPercentage", null).get().addOnSuccessListener {
+                val product = it.toObjects(Product::class.java)
+                viewModelScope.launch {
+                    _offerProduct.emit(Resource.Success(product))
+                }
+
+
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _offerProduct.emit(Resource.Error(it.message.toString()))
+                }
+
+            }
+
+
+    }
+
+    fun fetchBestsProduct() {
+        viewModelScope.launch {
+            _bestProduct.emit(Resource.Loading())
+        }
+        firestore.collection("Products").whereEqualTo("category", category.category)
+            .whereEqualTo("offerPercentage", null).get().addOnSuccessListener {
+                val product = it.toObjects(Product::class.java)
+                viewModelScope.launch {
+                    _bestProduct.emit(Resource.Success(product))
+                }
+
+
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _bestProduct.emit(Resource.Error(it.message.toString()))
+                }
+
+            }
+
+
+    }
+
+
+}
